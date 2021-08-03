c93c4f8f3fdd8213cddda27119be9380d403cc18
Tianqing Gao <tianqing.gao@student.adelaide.edu.au>
2021-08-03 20:52:39 +0930

    Add the learning material of EJS

---
+++ todolist/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+// this line of code set the address of where the users will access the resources.
+app.use(express.static('public'));
+
+var taskList = ["Task1","Task2","Task3"];
+
+
+app.get('/', (req, res) =>{
+    var today = new Date();
+    var options = {
+        weekday:"long",
+        day: "numeric",
+        month: "long",
+    };
+
+    var day = today.toLocaleDateString("en-US",options);
+
+    
+    // use render to send the variable to the front-end.
+    // in render the first para is the ejs filename the second is a dictionary which key is the varaiblename in the ejs file and the second is the one from the server side.
+    res.render('list',{dayType: day, taskList:taskList});
+});
+
+app.post('/', (req, res) =>{
+    taskList[taskList.length] = req.body.newTask;
+
+    res.redirect("/");
+})
+
+
+app.listen(3000,() => {
+    console.log("Server listening on port 3000");
+});
+
